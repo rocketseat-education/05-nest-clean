109350b084d4cf3c11dfe3ed73cafe6f4135b356
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-08-23 13:25:22 -0300

    Fundamentos do Nest - Módulos, serviços e controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hello')
+  index(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/hello')
+  store(): string {
+    return 'teste'
   }
 }
