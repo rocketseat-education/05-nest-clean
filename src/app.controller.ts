399b6cbec554d2f71c71674290d6e960d1e45cf9
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-08-23 13:52:22 -0300

    Fundamentos do Nest - Criando serviço do Prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
   index(): string {
@@ -11,7 +15,7 @@ export class AppController {
   }
 
   @Post('/hello')
-  store(): string {
-    return 'teste'
+  async store() {
+    return await this.prisma.user.findMany()
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
