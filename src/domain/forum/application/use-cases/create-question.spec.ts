da63fcfabc02d784642b95e66f8a4bdc48b0d17e
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-21 16:01:30 -0300

    Relacionamentos e uploads - Perguntas com anexos

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -37,4 +37,26 @@ describe('Create Question', () => {
       expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
     ])
   })
+
+  it('should persist attachments when creating a new question', async () => {
+    const result = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'Conteúdo da pergunta',
+      attachmentsIds: ['1', '2'],
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(2)
+    expect(inMemoryQuestionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1'),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1'),
+        }),
+      ]),
+    )
+  })
 })
--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -14,6 +14,18 @@ export class InMemoryQuestionAttachmentsRepository
     return questionAttachments
   }
 
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = questionAttachments
+  }
+
   async deleteManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
       (item) => item.questionId.toString() !== questionId,
