92f8d35d2acee8772ff2b6bcd2105301b8c73d2a
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-21 11:54:25 -0300

    Relacionamentos e uploads - Testando caso de uso de upload

---
+++ test/repositories/in-memory-attachments-repository.ts
@@ -0,0 +1,10 @@
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
+
+export class InMemoryAttachmentsRepository implements AttachmentsRepository {
+  public items: Attachment[] = []
+
+  async create(attachment: Attachment) {
+    this.items.push(attachment)
+  }
+}
---
+++ test/storage/fake-uploader.ts
@@ -0,0 +1,25 @@
+import {
+  UploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+import { randomUUID } from 'crypto'
+
+interface Upload {
+  fileName: string
+  url: string
+}
+
+export class FakeUploader implements Uploader {
+  public uploads: Upload[] = []
+
+  async upload({ fileName }: UploadParams): Promise<{ url: string }> {
+    const url = randomUUID()
+
+    this.uploads.push({
+      fileName,
+      url,
+    })
+
+    return { url }
+  }
+}
