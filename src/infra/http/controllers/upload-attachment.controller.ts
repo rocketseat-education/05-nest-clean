e5085b998dfc0333e93ff3fc824000b9ba8a1a0e
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-21 11:30:25 -0300

    Relacionamentos e uploads - Controller: Upload de arquivo

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+  // constructor() {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2mb
+          }),
+          new FileTypeValidator({
+            fileType: '.(png|jpg|jpeg|pdf)',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -38,6 +38,7 @@ import { FetchQuestionCommentsController } from './controllers/fetch-question-co
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
 import { FetchAnswerCommentsController } from './controllers/fetch-answer-comments.controller'
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
+import { UploadAttachmentController } from './controllers/upload-attachment.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -60,6 +61,7 @@ import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases
     DeleteAnswerCommentController,
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
+    UploadAttachmentController,
   ],
   providers: [
     CreateQuestionUseCase,
