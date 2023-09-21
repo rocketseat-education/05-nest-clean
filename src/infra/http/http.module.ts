9ae32bc66806d98e6d18e2ae4634701edbb6c42a
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-21 14:24:38 -0300

    Relacionamentos e uploads - Integração com Cloudflare R2

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -39,9 +39,11 @@ import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cas
 import { FetchAnswerCommentsController } from './controllers/fetch-answer-comments.controller'
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
 import { UploadAttachmentController } from './controllers/upload-attachment.controller'
+import { StorageModule } from '../storage/storage.module'
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule],
+  imports: [DatabaseModule, CryptographyModule, StorageModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
@@ -82,6 +84,7 @@ import { UploadAttachmentController } from './controllers/upload-attachment.cont
     DeleteAnswerCommentUseCase,
     FetchQuestionCommentsUseCase,
     FetchAnswerCommentsUseCase,
+    UploadAndCreateAttachmentUseCase,
   ],
 })
 export class HttpModule {}
---
+++ src/infra/storage/r2-storage.ts
@@ -0,0 +1,49 @@
+import {
+  UploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { Injectable } from '@nestjs/common'
+import { EnvService } from '../env/env.service'
+import { randomUUID } from 'node:crypto'
+
+@Injectable()
+export class R2Storage implements Uploader {
+  private client: S3Client
+
+  constructor(private envService: EnvService) {
+    const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID')
+
+    this.client = new S3Client({
+      endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+      region: 'auto',
+      credentials: {
+        accessKeyId: envService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: envService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    })
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: UploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID()
+    const uniqueFileName = `${uploadId}-${fileName}`
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get('AWS_BUCKET_NAME'),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    )
+
+    return {
+      url: uniqueFileName,
+    }
+  }
+}
