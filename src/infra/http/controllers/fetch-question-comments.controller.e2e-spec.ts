fb9a14e00cd17213d300e8c56d0d70bdd12ce293
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-22 11:36:03 -0300

    Relacionamentos e uploads - Controller: comentário com autor

--- src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -32,7 +32,9 @@ describe('Fetch question comments (E2E)', () => {
   })
 
   test('[GET] /questions/:questionId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe',
+    })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -63,8 +65,14 @@ describe('Fetch question comments (E2E)', () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
       comments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 01' }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
