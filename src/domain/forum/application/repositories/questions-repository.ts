cb4951713ea9db97dd0f11d08b0ed48fffb7d8ff
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-22 12:07:05 -0300

    Relacionamentos e uploads - Value Object: detalhes da pergunta

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,11 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract save(question: Question): Promise<void>
   abstract create(question: Question): Promise<void>
