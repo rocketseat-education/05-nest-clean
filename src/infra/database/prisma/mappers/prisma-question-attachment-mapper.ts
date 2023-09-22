ec950281bbfc513dfc844a8a116c8b1c1f62f1d4
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-22 10:06:00 -0300

    Relacionamentos e uploads - Criação de pergunta com anexos

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -21,7 +21,7 @@ export class PrismaQuestionAttachmentMapper {
     attachments: QuestionAttachment[],
   ): Prisma.AttachmentUpdateManyArgs {
     const attachmentIds = attachments.map((attachment) => {
-      return attachment.id.toString()
+      return attachment.attachmentId.toString()
     })
 
     return {
