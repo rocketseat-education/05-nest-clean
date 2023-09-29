064bbe4e92e46ea07810b352cce03382631bcf72
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 14:17:45 -0300

    Eventos de domínio & cache - Implementação do cache com Redis

---
+++ src/infra/cache/redis/redis-cache-repository.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common'
+import { CacheRepository } from '../cache-repository'
+import { RedisService } from './redis.service'
+
+@Injectable()
+export class RedisCacheRepository implements CacheRepository {
+  constructor(private redis: RedisService) {}
+
+  async set(key: string, value: string): Promise<void> {
+    await this.redis.set(key, value, 'EX', 60 * 15)
+  }
+
+  get(key: string): Promise<string | null> {
+    return this.redis.get(key)
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key)
+  }
+}
