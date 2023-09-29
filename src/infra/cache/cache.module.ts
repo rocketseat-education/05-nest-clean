182b1010dbc914800c52856b402dfd5f480f9fcd
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 14:17:45 -0300

    Eventos de domínio & cache - Implementação do cache com Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
+import { RedisService } from './redis/redis.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
--- src/infra/cache/redis/redis.service.ts
+++ src/infra/cache/redis/redis.service.ts
@@ -1,7 +1,8 @@
 import { EnvService } from '@/infra/env/env.service'
-import { OnModuleDestroy } from '@nestjs/common'
+import { Injectable, OnModuleDestroy } from '@nestjs/common'
 import { Redis } from 'ioredis'
 
+@Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
   constructor(envService: EnvService) {
     super({
