3ec5fbe6c406ba89fe68f6ce3c0db1ab8fb2f349
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 14:12:29 -0300

    Eventos de domínio & cache - Criando service do Redis

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -8,6 +8,9 @@ export const envSchema = z.object({
   AWS_BUCKET_NAME: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
+  REDIS_HOST: z.string().optional().default('127.0.0.1'),
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_DB: z.coerce.number().optional().default(0),
   PORT: z.coerce.number().optional().default(3333),
 })
 
