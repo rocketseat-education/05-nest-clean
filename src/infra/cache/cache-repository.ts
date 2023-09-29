4be27532d164c20e1d05b12aa81b4a0e92254fa8
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 14:03:26 -0300

    Eventos de domínio & cache - Integrando cache no Prisma

--- src/infra/cache/cache-repository.ts
+++ src/infra/cache/cache-repository.ts
@@ -1,5 +1,5 @@
 export abstract class CacheRepository {
   abstract set(key: string, value: string): Promise<void>
-  abstract get(key: string): Promise<void>
+  abstract get(key: string): Promise<string | null>
   abstract delete(key: string): Promise<void>
 }
