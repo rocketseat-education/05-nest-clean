c29b1ca5f92721d8b0a5346850fc5d3dcaa99f9b
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 13:21:58 -0300

    Eventos de domínio & cache - Testes E2E de eventos de domínio

--- test/utils/wait-for.ts
+++ test/utils/wait-for.ts
@@ -8,17 +8,17 @@
  * @param maxDuration Maximum wait time before rejecting
  */
 export async function waitFor(
-  assertions: () => void,
+  assertions: () => void | Promise<void>,
   maxDuration = 1000,
 ): Promise<void> {
   return new Promise((resolve, reject) => {
     let elapsedTime = 0
 
-    const interval = setInterval(() => {
+    const interval = setInterval(async () => {
       elapsedTime += 10
 
       try {
-        assertions()
+        await assertions()
         clearInterval(interval)
         resolve()
       } catch (err) {
